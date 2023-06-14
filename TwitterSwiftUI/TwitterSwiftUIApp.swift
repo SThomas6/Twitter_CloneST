89e1bcb8e745c66766e1283298b494d9413b36d8
steffanthomas <scthom06@gmail.com>
2023-06-14 01:33:21 +0100

    Initial Commit

---
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIApp.swift
+//  TwitterSwiftUI
+//
+//  Created by Steffan Thomas on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
